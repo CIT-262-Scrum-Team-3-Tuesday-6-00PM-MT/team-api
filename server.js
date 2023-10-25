fa9101c1fbae772dcbb33249f3eec180e827d473
lg10migue <lop18028@byui.edu>
2023-10-24 18:27:26 -0600

    Add my name endpoint.
    Added /miguel endpoint.

--- server.js
+++ server.js
@@ -10,5 +10,6 @@ app.get('/michael', (req,res) => {res.send("Hello Michael")});
 app.get('/john', (req,res) => {res.send("Hello John")});
 app.get('/cigi', (req,res) => {res.send("Hello Cigi")});
 app.get('/tadeo', (req,res) => {res.send("Hello Tadeo")});
+app.get('/miguel', (req,res) => {res.send("Hello Miguel!")});
 
 app.listen(port, ()=>console.log("Listening"));
