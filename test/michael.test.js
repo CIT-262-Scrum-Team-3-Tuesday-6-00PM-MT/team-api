fa8ecfec5cace2b8fc4e0ab7e7501ae8025f3895
Michael Anderson <darktrilarian@hotmail.com>
2023-10-30 21:41:55 -0500

    fixed endpoint

--- test/michael.test.js
+++ test/michael.test.js
@@ -3,5 +3,5 @@ const fetch = require( "node-fetch2" ) ;
 it( "Should match my name", async () => {
     const response = await fetch('http://localhost:3000/michael');
     const name = await response.text();
-    expect(name).toBe("michael");
+    expect(name).toBe("Hello Michael");
 } ) ;
