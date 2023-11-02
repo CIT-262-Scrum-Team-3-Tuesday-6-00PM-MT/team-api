f78a997cc3fb5dafe90786a1b3ca4aa886b0773f
lg10migue <lop18028@byui.edu>
2023-11-01 18:46:10 -0600

    Fixed Riley file.

--- test/riley.test.js
+++ test/riley.test.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch2");
 
 it ("Should match my name", async () => {
-    const response = await fetch("hettp://localhost:3000/riley")
+    const response = await fetch("http://localhost:3000/riley")
     const result = await response.text();
     expect(result).toBe("Hello Riley");
 });
