db1869a65e723727971aee614f54c4147a826591
Cigi <54045350+cigi801@users.noreply.github.com>
2023-11-01 18:13:31 -0600

    fixing Cigi's test

--- test/cigi.test.js
+++ test/cigi.test.js
@@ -1,7 +1,7 @@
 const fetch = require( "node-fetch2" ) ;
 
 it( "Should match my name", async () => {
-    const response = await fetch( "http://localhost:3000/miguel" ) ;
+    const response = await fetch( "http://localhost:3000/cigi" ) ;
     const result = await response.text() ;
     expect( result ).toBe( "Hello Cigi" ) ;
 } ) ;
