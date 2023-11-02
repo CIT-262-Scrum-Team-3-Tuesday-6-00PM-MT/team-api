2cc606d2b06f4c270107469ac2b073f183724fbb
Cigi <54045350+cigi801@users.noreply.github.com>
2023-11-01 18:11:04 -0600

    Adding Cigi's test

---
+++ test/cigi.test.js
@@ -0,0 +1,7 @@
+const fetch = require( "node-fetch2" ) ;
+
+it( "Should match my name", async () => {
+    const response = await fetch( "http://localhost:3000/miguel" ) ;
+    const result = await response.text() ;
+    expect( result ).toBe( "Hello Cigi" ) ;
+} ) ;
